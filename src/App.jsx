b821fc746e6fd63240884ee325c27c3bfd3d2db9
CHIKU BORAH <chiku.borah@w3villa.com>
2023-05-25 13:37:56 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/footer';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Header/>
+      <h1>New Page</h1>
+    </div> 
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
